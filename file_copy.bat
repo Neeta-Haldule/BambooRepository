d9487b39d624a6c779b4ffe941ee4716eb053017
Neeta-Haldule <halduleneeta@gmail.com>
2018-12-13 12:20:40 +0530

    Add files via upload

---
+++ file_copy.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set source=C:\Automation\Login_Logout\Execute_Suit\Output
+set dest=C:\Users\Administrator\bamboo-agent-home\xml-data\build-dir\EX-PLANTEST-TES
+pushd "%source%"
+for /f "tokens=*" %%G in ('dir *.xml /b /a-d /od') do SET newest=%%G
+
+copy "%newest%" "%dest%"
+popd
